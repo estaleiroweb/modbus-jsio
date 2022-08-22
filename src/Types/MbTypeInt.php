0f45008350ab75883a223ca1eea228bd2484eb40
Helbert Fernandes <helbert@estaleiroweb.com.br>
2022-08-22 20:18:15 -0300

    udp

--- src/Types/MbTypeInt.php
+++ src/Types/MbTypeInt.php
@@ -3,4 +3,17 @@
 namespace EstaleiroWeb\Modbus\Types;
 
 class MbTypeInt extends MbTypeAny {
+	public function __construct($val = null) {
+		$this->readonly['len'] = 2;
+		$this->readonly['unsigned'] = false;
+		$this->raw = $val;
+	}
+	public function setLen($val) {
+		$val = (int)$val;
+		if (in_array($val, [2, 4, 8])) {
+			$this->readonly['len'] = $val;
+			if ($this->aRaw) $this->raw = $this->raw;
+		}
+		return $this;
+	}
 }
